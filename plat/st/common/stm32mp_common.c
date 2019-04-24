1a52c03596e16d1827a8e26803560a57c3012e38
Yann Gautier <yann.gautier@st.com>
2019-04-24 13:38:41 +0200

    feat(st): protect access on PWR registers
    In SMP context, we need to add spinlocks to protect against concurrent
    accesses on PWR registers.
    
    Change-Id: I27cb698ffa085eca7b61b042ce5ccaa1fd2daaf4
    Signed-off-by: Yann Gautier <yann.gautier@st.com>

--- plat/st/common/stm32mp_common.c
+++ plat/st/common/stm32mp_common.c
@@ -16,6 +16,7 @@
 #include <drivers/st/stm32mp_reset.h>
 #include <lib/mmio.h>
 #include <lib/smccc.h>
+#include <lib/spinlock.h>
 #include <lib/xlat_tables/xlat_tables_v2.h>
 #include <plat/common/platform.h>
 #include <services/arm_arch_svc.h>
@@ -56,6 +57,7 @@
 #define BOOT_INST_SHIFT			8
 
 static console_t console;
+static struct spinlock lock;
 
 uintptr_t plat_get_ns_image_entrypoint(void)
 {
@@ -118,6 +120,20 @@ bool stm32mp_lock_available(void)
 #else
 	return (read_sctlr() & c_m_bits) == c_m_bits;
 #endif
+}
+
+void stm32mp_pwr_regs_lock(void)
+{
+	if (stm32mp_lock_available()) {
+		spin_lock(&lock);
+	}
+}
+
+void stm32mp_pwr_regs_unlock(void)
+{
+	if (stm32mp_lock_available()) {
+		spin_unlock(&lock);
+	}
 }
 
 int stm32mp_map_ddr_non_cacheable(void)
