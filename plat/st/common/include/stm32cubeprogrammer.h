5560018d91c2679c99065a58e8fcb092f08376c4
Lionel Debieve <lionel.debieve@st.com>
2020-11-10 09:21:08 +0100

    feat(st): add SSP support for STM32CubeProgrammer
    NOT_UPSTREAMABLE
    
    Add STM32CubeProgrammer communication update for SSP support.
    
    Signed-off-by: Patrick Delaunay <patrick.delaunay@st.com>
    Change-Id: I1830bff82d87b3535f0787eb5f47c56960feccaa
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/tf-a/+/185335
    Reviewed-by: CITOOLS <smet-aci-reviews@lists.codex.cro.st.com>
    Reviewed-by: Lionel DEBIEVE <lionel.debieve@st.com>
    Reviewed-by: Yann GAUTIER <yann.gautier@st.com>
    Tested-by: Lionel DEBIEVE <lionel.debieve@st.com>

--- plat/st/common/include/stm32cubeprogrammer.h
+++ plat/st/common/include/stm32cubeprogrammer.h
@@ -16,6 +16,7 @@
 #define PHASE_DDR_FW		2U
 #define PHASE_SSBL		3U
 #define PHASE_CMD		0xF1U
+#define PHASE_SSP		0xF3U
 #define PHASE_RESET		0xFFU
 
 /* Functions provided by plat */
@@ -28,5 +29,17 @@ int stm32cubeprog_usb_load(struct usb_handle *usb_core_handle,
 
 int stm32cubeprog_uart_load(uintptr_t instance, uint8_t phase,
 			   uintptr_t base, size_t len);
+
+int stm32cubeprog_usb_ssp(struct usb_handle *usb_core_handle,
+			  uintptr_t cert_base,
+			  size_t cert_len,
+			  uintptr_t ssp_base,
+			  size_t ssp_len);
+
+int stm32cubeprog_uart_ssp(uintptr_t instance,
+			   uintptr_t cert_base,
+			   size_t cert_len,
+			   uintptr_t ssp_base,
+			   size_t ssp_len);
 
 #endif /* STM32CUBEPROGRAMMER_H */
