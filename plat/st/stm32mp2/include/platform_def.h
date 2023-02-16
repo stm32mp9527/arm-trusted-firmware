89c935cb8b0f1e8875dbc17565cb5cc93daf3c09
Patrick Delaunay <patrick.delaunay@foss.st.com>
2023-02-16 11:33:17 +0100

    feat(stm32mp2): add support for PSCI_OS_INIT_MODE
    Enable PSCI_OS_INIT_MODE support for stm32mp2 with 5 levels
    in the topology:
    
    0: (2) CPU1 core#0 / CPU1 core#1
    1: (1) CLUSTER / D1 Domain
    2: (1) LPLV D1
    3: (1) D2
    4: (1) LPLV D2 : SoC
    
    The 5 power levels allow to handle LPLV-STOP specific treatment in
    SPD OP-TEE with cpu_suspend_entry parameter max_off_pwrlvl.
    
    The PSCI PM idle state uses the recommended encoding for State-ID
    in PSCI 1.0 Specification, with extended State-ID formats,
    PSCI_EXTENDED_STATE_ID is activated.
    
    See also ARM_RECOM_STATE_ID_ENC for ARM platform.
    
    PSCI_CPU_SUSPEND request is handled for the low power modes
    STOP1/2 LP-STOP1/2 and LPLVSTOP1/2
    and PSCI_SYSTEM_SUSPEND request for STANDBY mode.
    
    Allow board to reduce the list of supported state IDs for PSCI suspend
    request, CPU or system.
    
    This feature is useful for board without PMIC, when the supported
    modes are limited by the regulators present on the board. For example
    when the 2 LPLV modes are not supported as the associated regulators are
    fixed.
    
    To remove a mode, the associated sub-node in domain-idle-states node
    should be removed from the board device tree.
    
    Signed-off-by: Patrick Delaunay <patrick.delaunay@foss.st.com>
    Change-Id: I392f459458c0d8e030b83a5b9ed392fb8cd20ed9
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/tf-a/+/301446
    ACI: CITOOLS <MDG-smet-aci-reviews@list.st.com>
    Reviewed-by: Yann GAUTIER <yann.gautier@foss.st.com>
    Domain-Review: Yann GAUTIER <yann.gautier@foss.st.com>

--- plat/st/stm32mp2/include/platform_def.h
+++ plat/st/stm32mp2/include/platform_def.h
@@ -33,9 +33,8 @@
 #define PLATFORM_CORE_COUNT		U(2)
 #define PLATFORM_MAX_CPUS_PER_CLUSTER	U(2)
 
-#define PLAT_MAX_PWR_LVL		U(5)
-#define PLAT_MAX_CPU_SUSPEND_PWR_LVL	U(5)
-#define PLAT_NUM_PWR_DOMAINS		U(7)
+#define PLAT_MAX_PWR_LVL		U(4)
+#define PLAT_NUM_PWR_DOMAINS		U(6)
 
 /* Local power state for power domains in Run state. */
 #define STM32MP_LOCAL_STATE_RUN		U(0)
@@ -178,27 +177,5 @@
 	INTR_PROP_DESC(ARM_IRQ_SEC_SGI_6,		\
 		       GIC_HIGHEST_SEC_PRIORITY,	\
 		       (grp), GIC_INTR_CFG_EDGE)
-
-/*
- * Power
- */
-#define PLAT_MAX_PWR_LVL	U(1)
-
-/* Local power state for power domains in Run state. */
-#define ARM_LOCAL_STATE_RUN	U(0)
-/* Local power state for retention. Valid only for CPU power domains */
-#define ARM_LOCAL_STATE_RET	U(1)
-/* Local power state for power-down. Valid for CPU and cluster power domains */
-#define ARM_LOCAL_STATE_OFF	U(2)
-/*
- * This macro defines the deepest retention state possible.
- * A higher state id will represent an invalid or a power down state.
- */
-#define PLAT_MAX_RET_STATE		ARM_LOCAL_STATE_RET
-/*
- * This macro defines the deepest power down states possible. Any state ID
- * higher than this is invalid.
- */
-#define PLAT_MAX_OFF_STATE		ARM_LOCAL_STATE_OFF
 
 #endif /* PLATFORM_DEF_H */
