6f45be65660d3f1101fcdf3377f6e461bd00b323
Yann Gautier <yann.gautier@foss.st.com>
2023-09-12 17:26:17 +0200

    feat(stm32mp2): stub DDR configuration when not TDCID
    If Cortex-A35 is not TDCID, the DDR configuration will be done by
    Cortex-M33. The DDR driver is then not compiled in that case, and put
    under STM32MP_M33_TDCID flag.
    
    Signed-off-by: Yann Gautier <yann.gautier@foss.st.com>
    Change-Id: Ib8326271a83b0d5b575ac0f723831af2c91c1b9a

--- plat/st/stm32mp2/platform.mk
+++ plat/st/stm32mp2/platform.mk
@@ -54,6 +54,7 @@ DWL_BUFFER_BASE 	?=	0x87000000
 STM32MP_DDR3_TYPE	?=	0
 STM32MP_DDR4_TYPE	?=	0
 STM32MP_LPDDR4_TYPE	?=	0
+ifeq ($(STM32MP_M33_TDCID),0)
 ifeq (${STM32MP_DDR3_TYPE},1)
 DDR_TYPE		:=	ddr3
 endif
@@ -67,6 +68,10 @@ endif
 # DDR features
 STM32MP_DDR_DUAL_AXI_PORT	:=	1
 STM32MP_DDR_FIP_IO_STORAGE	:=	1
+else #STM32MP_M33_TDCID
+STM32MP_DDR_DUAL_AXI_PORT	:=	0
+STM32MP_DDR_FIP_IO_STORAGE	:=	0
+endif #STM32MP_M33_TDCID
 
 # Device tree
 BL2_DTSI			:=	stm32mp25-bl2.dtsi
@@ -232,6 +237,7 @@ BL2_SOURCES			+=	drivers/st/usb_dwc3/usb_dwc3.c				\
 					plat/st/stm32mp2/stm32mp2_usb_dfu.c
 endif
 
+ifeq ($(STM32MP_M33_TDCID),0)
 BL2_SOURCES			+=	drivers/st/ddr/stm32mp2_ddr.c				\
 					drivers/st/ddr/stm32mp2_ddr_helpers.c			\
 					drivers/st/ddr/stm32mp2_ram.c
@@ -261,6 +267,7 @@ BL2_SOURCES			+=	drivers/st/ddr/phy/phyinit/src/ddrphy_phyinit_d_loadimem.c				\
 					drivers/st/ddr/phy/phyinit/src/ddrphy_phyinit_writeoutmem.c				\
 					drivers/st/ddr/phy/phyinit/usercustom/ddrphy_phyinit_usercustom_h_readmsgblock.c 	\
 					drivers/st/ddr/phy/phyinit/usercustom/ddrphy_phyinit_usercustom_g_waitfwdone.c
+endif #STM32MP_M33_TDCID
 
 # BL31 sources
 BL31_SOURCES			+=	${FDT_WRAPPERS_SOURCES}
@@ -286,8 +293,9 @@ BL31_SOURCES		+=	plat/st/common/stm32mp_svc_setup.c			\
 BL31_SOURCES		+=	services/arm_arch_svc/arm_arch_svc_setup.c
 
 # Compilation rules
-.PHONY: check_ddr_type
 .SUFFIXES:
+ifeq ($(STM32MP_M33_TDCID),0)
+.PHONY: check_ddr_type
 
 bl2: check_ddr_type
 
@@ -299,6 +307,7 @@ check_ddr_type:
 		echo "One and only one DDR type must be defined"; \
 		false; \
 	fi
+endif #STM32MP_M33_TDCID
 
 # generate separate DDR FIP image
 ifeq (${STM32MP_DDR_FIP_IO_STORAGE},1)
