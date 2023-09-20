d5d599bfd47b79c3ce95fd42c8e5dc5446b97a66
Yann Gautier <yann.gautier@foss.st.com>
2023-09-20 16:15:49 +0200

    feat(stm32mp2): add OTP functions M33 TDCID
    Add new functions stm32_otp_* functions in a dedicated file, for the
    case where Cortex-M33 is TDCID, and then BSEC cannot be accessed.
    For the moment, they are empty and just return BSEC_OK.
    
    Change-Id: Ic51524122e122e53853173bae4bb260616a2c83d
    Signed-off-by: Yann Gautier <yann.gautier@foss.st.com>

--- plat/st/stm32mp2/platform.mk
+++ plat/st/stm32mp2/platform.mk
@@ -182,8 +182,13 @@ PLAT_BL_COMMON_SOURCES		+=	drivers/st/i2c/stm32_i2c.c
 
 PLAT_BL_COMMON_SOURCES		+=	plat/st/stm32mp2/stm32mp2_private.c
 
-PLAT_BL_COMMON_SOURCES		+=	drivers/st/bsec/bsec3.c					\
-					drivers/st/iwdg/stm32_iwdg.c				\
+ifeq ($(STM32MP_M33_TDCID),0)
+PLAT_BL_COMMON_SOURCES		+=	drivers/st/bsec/bsec3.c
+else
+PLAT_BL_COMMON_SOURCES		+=	plat/st/stm32mp2/stm32mp2_otp.c
+endif
+
+PLAT_BL_COMMON_SOURCES		+=	drivers/st/iwdg/stm32_iwdg.c				\
 					drivers/st/reset/stm32mp2_reset.c			\
 					plat/st/stm32mp2/stm32mp2_context.c			\
 					plat/st/stm32mp2/stm32mp2_pwr.c				\
