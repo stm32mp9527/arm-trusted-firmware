fcaf2ffab58866d2765397bd5c30ffb6cc079d6c
Yann Gautier <yann.gautier@foss.st.com>
2024-07-03 19:12:30 +0200

    feat(stm32mp2): display package type for STM32MP23
    The package type is read from OTP fuse. Display it with other SoC
    information at boot.
    
    Change-Id: I855bd1ac8d96ca88a7e4ee5ac8b6f65697214cc9
    Signed-off-by: Yann Gautier <yann.gautier@foss.st.com>
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/tf-a/+/391283
    ACI: CIBUILD <MDG-smet-aci-builds@list.st.com>
    ACI: CITOOLS <MDG-smet-aci-reviews@list.st.com>
    Reviewed-by: Maxime MERE <maxime.mere@foss.st.com>

--- plat/st/stm32mp2/stm32mp2_private.c
+++ plat/st/stm32mp2/stm32mp2_private.c
@@ -399,6 +399,20 @@ void stm32mp_get_soc_name(char name[STM32_SOC_NAME_SIZE])
 		pkg = "AM";
 		break;
 #endif /* STM32MP21 */
+#if STM32MP23
+	case STM32MP23_PKG_CUSTOM:
+		pkg = "XX";
+		break;
+	case STM32MP23_PKG_AL_VFBGA361:
+		pkg = "AL";
+		break;
+	case STM32MP23_PKG_AK_VFBGA424:
+		pkg = "AK";
+		break;
+	case STM32MP23_PKG_AJ_TFBGA361:
+		pkg = "AJ";
+		break;
+#endif /* STM32MP23 */
 #if STM32MP25
 	case STM32MP25_PKG_CUSTOM:
 		pkg = "XX";
