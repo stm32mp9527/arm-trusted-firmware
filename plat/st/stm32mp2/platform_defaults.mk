e25d0387316f4d651454530e504d0a6839d8fb08
Maxime Méré <maxime.mere@foss.st.com>
2024-12-13 14:12:32 +0100

    feat(stm32mp2): enable LTO support in STM32MP2
    Allow the usage of link time optimization in STM32MP2 platform and
    enable it. When enabled, PIE must be disabled as bl2 and bl31 base
    address must be asolute otherwise some ASM function will fail.
    
    Change-Id: Ib5194ac10501e20c06dc646c1f6e2e24039c0637
    Signed-off-by: Maxime Méré <maxime.mere@foss.st.com>
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/tf-a/+/423952
    ACI: CITOOLS <MDG-smet-aci-reviews@list.st.com>
    Domain-Review: Yann GAUTIER <yann.gautier@foss.st.com>

---
+++ plat/st/stm32mp2/platform_defaults.mk
@@ -0,0 +1,8 @@
+#
+# Copyright (c) 2025, STMicroelectronics - All Rights Reserved
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+# LTO is enabled on this platform due to memory constraints
+ENABLE_LTO			:= 1
