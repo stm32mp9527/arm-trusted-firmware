stm32mp9527/arm-trusted-firmware

commit 11d21feae7cb62beee17940bd097d148f5fe4fb7

feat(stm32mp2): force autonomous mode for LPSRAM1/2/3 when CPU3 is used
Force the autonomous mode for LPSRAM1/2/3 with AMEN bit in RCC_LPSRAMxCFGR
when CPU3=M0 is running and when it is needed for low power mode,
in Standby or in LPLV-Stop because its firmware is running in LPSRAMx.

Change-Id: I4fb272e41b09dc77ace0c2bcadefba8a0efb134e
Signed-off-by: Patrick Delaunay <patrick.delaunay@foss.st.com>
Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/tf-a/+/405969
Reviewed-by: Maxime MERE <maxime.mere@foss.st.com>
ACI: CITOOLS <MDG-smet-aci-reviews@list.st.com>
Domain-Review: Thomas BOURGOIN <thomas.bourgoin@foss.st.com>
Tested-by: Maxime MERE <maxime.mere@foss.st.com>

plat/st/stm32mp2/stm32mp2_pm.c
@@ -91,6 +91,11 @@ static u_register_t saved_scr_el3;
 static uint32_t lpstop1_pwrlpdly;
 static uint32_t stop2_pwrlpdly;
 
+#if !STM32MP21
+/* bitfield to indicate the modified AMEN bit for LP-SRAM1/2/3 */
+static uint32_t saved_lpsram_amen;
+#endif
+
 /* Support PSCI v1.0 Extended State-ID with the recommended encoding */
 #define LVL_CORE		U(0)
 #define LVL_D1			U(1)
@@ -237,6 +242,61 @@ static void stm32mp_ca35_lpi_restore(void)
 	while ((mmio_read_32(A35SSC_BASE + CA35SS_SSC_LPI_STGEN_NTS(WS1)) & STGEN_CSYSACK) == 0U) {
 		;
 	}
+}
+
+/* LPSRAM1/2/3 autonomous support (AMEN) when D3 is used in low power */
+static void lpsram_autonomous_mode_set(uintptr_t rcc_base)
+{
+#if !STM32MP21 /* STM32MP21 Series don't have D3 domain */
+	const uint32_t mask = RCC_C3CFGR_C3EN | RCC_C3CFGR_C3AMEN;
+
+	/* configure LPSRAM only when C3 is used in autonoumous mode */
+	if ((mmio_read_32(rcc_base + RCC_C3CFGR) & mask) != mask) {
+		return;
+	}
+
+	saved_lpsram_amen = 0U;
+
+	/* force autonomous mode (ANEN) when clock is enabled (EN) and ANEN is not yet set */
+	if ((mmio_read_32(rcc_base + RCC_LPSRAM1CFGR) &
+	     (RCC_LPSRAM1CFGR_LPSRAM1EN | RCC_LPSRAM1CFGR_LPSRAM1AMEN)) ==
+	      RCC_LPSRAM1CFGR_LPSRAM1EN) {
+		mmio_setbits_32(rcc_base + RCC_LPSRAM1CFGR,
+				RCC_LPSRAM1CFGR_LPSRAM1AMEN);
+		saved_lpsram_amen |=  BIT(0);
+	}
+	if ((mmio_read_32(rcc_base + RCC_LPSRAM2CFGR) &
+	     (RCC_LPSRAM2CFGR_LPSRAM2EN | RCC_LPSRAM2CFGR_LPSRAM2AMEN)) ==
+	      RCC_LPSRAM2CFGR_LPSRAM2EN) {
+		mmio_setbits_32(rcc_base + RCC_LPSRAM2CFGR,
+				RCC_LPSRAM2CFGR_LPSRAM2AMEN);
+		saved_lpsram_amen |=  BIT(1);
+	}
+	if ((mmio_read_32(rcc_base + RCC_LPSRAM3CFGR) &
+	     (RCC_LPSRAM3CFGR_LPSRAM3EN | RCC_LPSRAM3CFGR_LPSRAM3AMEN)) ==
+	      RCC_LPSRAM3CFGR_LPSRAM3EN) {
+		mmio_setbits_32(rcc_base + RCC_LPSRAM3CFGR,
+				RCC_LPSRAM3CFGR_LPSRAM3AMEN);
+		saved_lpsram_amen |=  BIT(2);
+	}
+#endif /* !STM32MP21 */
+}
+
+static void lpsram_autonomous_mode_restore(uintptr_t rcc_base)
+{
+#if !STM32MP21
+	if (saved_lpsram_amen & BIT(0))
+		mmio_clrbits_32(rcc_base + RCC_LPSRAM1CFGR,
+				RCC_LPSRAM1CFGR_LPSRAM1AMEN);
+	if (saved_lpsram_amen & BIT(1))
+		mmio_clrbits_32(rcc_base + RCC_LPSRAM2CFGR,
+				RCC_LPSRAM2CFGR_LPSRAM2AMEN);
+	if (saved_lpsram_amen & BIT(2))
+		mmio_clrbits_32(rcc_base + RCC_LPSRAM3CFGR,
+				RCC_LPSRAM3CFGR_LPSRAM3AMEN);
+
+	saved_lpsram_amen = 0U;
+#endif /* !STM32MP21 */
 }
 
 /*******************************************************************************
@@ -518,6 +578,7 @@ static void stm32_pwr_domain_suspend(const psci_power_state_t *target_state)
 
 	case PWRSTATE_LPLV_STOP1:
 		print_mode_verbose("LPLV_Stop1");
+		lpsram_autonomous_mode_set(rcc_base);
 		mmio_write_32(pwr_base + PWR_CPU1CR, PWR_CPU1CR_LPDS_D1 | PWR_CPU1CR_LVDS_D1);
 		if (!cpu2_running) {
 			mmio_write_32(pwr_base + PWR_CPU2CR,
@@ -552,6 +613,7 @@ static void stm32_pwr_domain_suspend(const psci_power_state_t *target_state)
 
 	case PWRSTATE_LPLV_STOP2:
 		print_mode_info("LPLV_Stop2");
+		lpsram_autonomous_mode_set(rcc_base);
 		mmio_write_32(pwr_base + PWR_CPU1CR, PWR_CPU1CR_PDDS_D1);
 		if (!cpu2_running) {
 			mmio_write_32(pwr_base + PWR_CPU2CR,
@@ -565,6 +627,7 @@ static void stm32_pwr_domain_suspend(const psci_power_state_t *target_state)
 
 	case PWRSTATE_STANDBY:
 		print_mode_info("Standby1");
+		lpsram_autonomous_mode_set(rcc_base);
 		mmio_write_32(pwr_base + PWR_CPU1CR, PWR_CPU1CR_PDDS_D1 | PWR_CPU1CR_PDDS_D2);
 		if (!cpu2_running) {
 			mmio_write_32(pwr_base + PWR_CPU2CR, PWR_CPU2CR_PDDS_D2);
@@ -717,6 +780,8 @@ static void stm32_pwr_domain_suspend_finish(const psci_power_state_t
 
 	/* Restore SCR */
 	write_scr_el3(saved_scr_el3);
+
+	lpsram_autonomous_mode_restore(rcc_base);
 
 	/* Core synchronization to protect DDR access */
 	stm32mp_state_set(STM32MP_PRIMARY_CPU, STATE_DDR, true);

commit dc7de12f3d3dda6e468275e48dddbd447a11a10d

fix(stm32mp2): save lpsram_amen in low-power context
Save lpsram_amen during Standby in pm context, so the associated register
are restored correctly on next wake-up.

Fixes: 11d21feae7cb ("feat(stm32mp2): force autonomous mode for LPSRAM1/2/3 when CPU3 is used")
Signed-off-by: Patrick Delaunay <patrick.delaunay@foss.st.com>
Change-Id: I28afa76cab9668169a37812da87126975addc516
Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/tf-a/+/419792
Tested-by: Maxime MERE <maxime.mere@foss.st.com>
Domain-Review: Maxime MERE <maxime.mere@foss.st.com>
Reviewed-by: Maxime MERE <maxime.mere@foss.st.com>

plat/st/stm32mp2/bl31_plat_setup.c
@@ -146,7 +146,7 @@ void bl31_plat_arch_setup(void)
 		/* Initialize the runtime services e.g. PSCI. */
 		runtime_svc_init();
 
-		stm32_pm_context_restore();
+		stm32_pm_context_init();
 
 		/* Jump manually to BL31 warm entry point, with MMU disabled. */
 		dsbsy();

plat/st/stm32mp2/include/stm32mp2_context.h
@@ -14,9 +14,10 @@
 void stm32mp_pm_save_enc_mkey_in_context(uint8_t *data);
 void stm32mp_pm_get_enc_mkey_from_context(uint8_t *data);
 
+void stm32_pm_context_init(void);
 bool stm32_pm_context_is_valid(void);
-void stm32_pm_context_save(const psci_power_state_t *state);
-void stm32_pm_context_restore(void);
+void stm32_pm_context_save(const psci_power_state_t *state, const void *data, size_t size);
+void stm32_pm_context_restore(void *data, size_t size);
 void stm32_pm_context_clear(void);
 
 #endif /* STM32MP2_CONTEXT_H */

plat/st/stm32mp2/stm32mp2_context.c
@@ -21,6 +21,9 @@
 #define BACKUP_CTX_CLK		CK_BUS_BKPSRAM
 #define ENC_KEY_SIZE_IN_BYTES	RISAF_KEY_SIZE_IN_BYTES
 
+/* Size of data for caller, the PM functions */
+#define DATA_SIZE		U(4)
+
 /* Magic used to indicated valid = ' ' 'M' 'P' '2' */
 #define CONTEXT_MAGIC			0x204D5032
 
@@ -36,6 +39,7 @@ struct backup_data_s {
 	optee_context_t opteed_sp_context[OPTEED_CORE_COUNT];
 #endif
 	uintptr_t fdt_bl31;
+	uint8_t data[DATA_SIZE];
 };
 
 void stm32mp_pm_save_enc_mkey_in_context(uint8_t *data)
@@ -79,7 +83,7 @@ bool stm32_pm_context_is_valid(void)
 	return ret;
 }
 
-void stm32_pm_context_save(const psci_power_state_t *state)
+void stm32_pm_context_save(const psci_power_state_t *state, const void *data, size_t size)
 {
 	void *cpu_context;
 	struct backup_data_s *backup_data;
@@ -118,10 +122,15 @@ void stm32_pm_context_save(const psci_power_state_t *state)
 	fdt_get_address(&fdt);
 	backup_data->fdt_bl31 = (uintptr_t)fdt;
 
+	/* Copy other data in Backup SRAM */
+	assert(size <= (size_t)DATA_SIZE);
+	if ((data != NULL) && (size != 0U)) {
+		(void)memcpy(&backup_data->data, data, MIN(size, (size_t)DATA_SIZE));
+	}
 	clk_disable(BACKUP_CTX_CLK);
 }
 
-void stm32_pm_context_restore(void)
+void stm32_pm_context_restore(void *data, size_t size)
 {
 	void *cpu_context;
 	struct backup_data_s *backup_data;
@@ -164,6 +173,10 @@ void stm32_pm_context_restore(void)
 	if (ret < 0) {
 		ERROR("%s: failed to open DT (%d)\n", __func__, ret);
 		panic();
+	}
+
+	if ((data != NULL) && (size != 0U)) {
+		(void)memcpy(data, (const void *)&backup_data->data, MIN(size, (size_t)DATA_SIZE));
 	}
 
 	clk_disable(BACKUP_CTX_CLK);

plat/st/stm32mp2/stm32mp2_pm.c
@@ -92,9 +92,15 @@ static u_register_t saved_scr_el3;
 static uint32_t lpstop1_pwrlpdly;
 #endif
 
-#if !STM32MP21 && !STM32MP_M33_TDCID
+/* PM data saved in pm context during Standby */
+#if STM32MP21 || STM32MP_M33_TDCID
+#define PM_CTX_DATA	NULL
+#define PM_CTX_SIZE	U(0)
+#else
 /* bitfield to indicate the modified AMEN bit for LP-SRAM1/2/3 */
 static uint32_t saved_lpsram_amen;
+#define PM_CTX_DATA	&saved_lpsram_amen
+#define PM_CTX_SIZE	sizeof(saved_lpsram_amen)
 #endif
 
 /* Support PSCI v1.0 Extended State-ID with the recommended encoding */
@@ -869,7 +875,7 @@ static void __dead2 stm32_pwr_domain_pwr_down_wfi(const psci_power_state_t
 	if (psci_is_last_on_cpu_safe() &&
 	    stm32_get_stateid(target_state->pwr_domain_state) == PWRSTATE_STANDBY) {
 		/* Save the context when all the core are requested to stop */
-		stm32_pm_context_save(target_state);
+		stm32_pm_context_save(target_state, PM_CTX_DATA, PM_CTX_SIZE);
 	}
 
 #if !STM32MP_M33_TDCID
@@ -1449,6 +1455,14 @@ int plat_setup_psci_ops(uintptr_t sec_entrypoint,
 	*psci_ops = &stm32_psci_ops;
 
 	return 0;
+}
+
+/*******************************************************************************
+ * PM init after Standby
+ ******************************************************************************/
+void stm32_pm_context_init(void)
+{
+	stm32_pm_context_restore(PM_CTX_DATA, PM_CTX_SIZE);
 }
 
 /*